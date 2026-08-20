e8d77e8e118286df17384650541b4955909d7d9a
Mohammed Munawwar <munawwar.ahmed61@gmail.com>
2026-08-20 20:06:24 +0530

    s3 bucket region change

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket       = "munawwar-tfstate-2026"
     key          = "prod/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
   }
