878b5298ffabf1c94088a3ae97b081e7cc3908c8
Mohammed Munawwar <munawwar.ahmed61@gmail.com>
2026-08-20 20:17:00 +0530

    Change Terraform region to us-east-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,12 +9,12 @@ terraform {
   backend "s3" {
     bucket       = "munawwar-tfstate-2026"
     key          = "prod/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
