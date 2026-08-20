6fe3efc6517b4657deb367a30d94ee671a742107
Mohammed Munawwar <munawwar.ahmed61@gmail.com>
2026-08-20 18:25:53 +0530

    Add Terraform AWS pipeline with S3 backend

---
+++ terraform/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "munawwar-tfstate-2026"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "test" {
+  bucket = "munawwar-test-bucket-2026"
+}
