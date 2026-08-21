85f1b40079f1e918250ee21d70bd6b722b5198f3
Munawwar <munawwar.ahmed61@gmail.com>
2026-08-21 16:26:59 +0530

    create ec2 with name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
-/*resource "aws_instance" "myec2" {
+resource "aws_instance" "myec2" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
+   
+    tags = {
+    Name        = "new-instance-name"   # Changes the display name in the AWS console
+  }
 }
-*/
+
+
